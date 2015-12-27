306c7321af62df5a53d4e1d481b7dda3baa68f0d
partizanes <partizanes1@gmail.com>
2015-12-28 01:37:58 +0300

    [3] add gallery frame, js check mobile client and add background to main page,

--- assets/js/examples.js
+++ assets/js/examples.js
@@ -1,11 +1,35 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
 
-	$(function() {
-		$("a[href$=hash]").addClass('active');
-	});
+    if(isMobile.any()){
+        var cssObj = {
+            'width' : '0%',
+            'height' : '0%'};
+        $('video').css(cssObj);
+    }
 
 	$('#fullpage').fullpage({
 		verticalCentered: true,
@@ -15,10 +39,89 @@ $(document).ready(function(){
 		loopTop: true,
 		loopBottom: true,
         slidesNavigation: true,
-		afterRender: function(){
-			//playing the video
-			$('video').get(0).play();
-		}
+        afterRender: function () {
+            $('video').get(0).play();
+        }
 	});
+
+    $(".polaroid").each(function (i) {
+        var tempVal = Math.round(Math.random());
+        if(tempVal == 1) {
+            var rotDegrees = randomXToY(-20, 20); // rotate left
+        } else {
+            var rotDegrees = randomXToY(-15, 15); // rotate right
+        }
+
+        // Internet Explorer doesn't have the "window.innerWidth" and "window.innerHeight" properties
+        if(window.innerWidth == undefined) {
+            var wiw = 1000;
+            var wih = 700;
+        } else {
+            var wiw = window.innerWidth;
+            var wih = window.innerHeight;
+        }
+
+        var cssObj = { 'left' : Math.random()*(wiw-400),
+            'top' : Math.random()*(wih-400),
+            '-webkit-transform' : 'rotate('+ rotDegrees +'deg)',  // safari only
+            'tranform' : 'rotate('+ rotDegrees +'deg)' }; // added in case CSS3 is standard
+        $(this).css(cssObj);
+    });
+
+    // Set the Z-Index (used to display images on top while dragging)
+    var zindexnr = 1;
+
+    // boolean to check if the user is dragging
+    var dragging = false;
+
+    // Show the polaroid on top when clicked on
+    $(".polaroid").mouseup(function(e){
+        if(!dragging) {
+            // Bring polaroid to the foreground
+            zindexnr++;
+            var cssObj = { 'z-index' : zindexnr,
+                'transform' : 'rotate(0deg)',	 // added in case CSS3 is standard
+                '-webkit-transform' : 'rotate(0deg)' };  // safari only
+            $(this).css(cssObj);
+        }
+    });
+
+    // Make the polaroid draggable & display a shadow when dragging
+    $(".polaroid").draggable({
+        cursor: 'crosshair',
+        start: function(event, ui) {
+            dragging = true;
+            zindexnr++;
+            var cssObj = { 'box-shadow' : '#888 5px 10px 10px', // added in case CSS3 is standard
+                '-webkit-box-shadow' : '#888 5px 10px 10px', // safari only
+                'margin-left' : '-10px',
+                'margin-top' : '-10px',
+                'z-index' : zindexnr };
+            $(this).css(cssObj);
+        },
+        stop: function(event, ui) {
+            var tempVal = Math.round(Math.random());
+            if(tempVal == 1) {
+                var rotDegrees = randomXToY(-20, 20); // rotate left
+            } else {
+                var rotDegrees = randomXToY(-15, 15); // rotate right
+            }
+            var cssObj = { 'box-shadow' : '', // added in case CSS3 is standard
+                '-webkit-box-shadow' : '', // safari only
+                'transform' : 'rotate('+ rotDegrees +'deg)', // added in case CSS3 is standard
+                '-webkit-transform' : 'rotate('+ rotDegrees +'deg)', // safari only
+                'margin-left' : '0px',
+                'margin-top' : '0px' };
+            $(this).css(cssObj);
+            dragging = false;
+        }
+    });
+
+    // Function to get random number upto m
+
+    function randomXToY(minVal,maxVal,floatVal) {
+        var randVal = minVal+(Math.random()*(maxVal-minVal));
+        return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
+    }
 });
 
