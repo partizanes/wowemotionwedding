cd4f124c2b9d6559a4bdfd07dde8ad407f1dd949
partizanes <partizanes1@gmail.com>
2016-01-07 22:10:43 +0300

    [11] fix background video in main page placed in center.add margin top.cleanup;

--- assets/js/examples.js
+++ assets/js/examples.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
         $('video').css(cssObj);
     }
 
-	$('#fullpage').fullpage({
+    $('#fullpage').fullpage({
 		verticalCentered: true,
 		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#1bbc9b', '#4BBFC3'],
 		anchors: ['about', 'gallery', 'blog' , 'reviews' , 'contacts'],
@@ -49,6 +49,7 @@ $(document).ready(function(){
             $('video').get(0).play();
         }
 	});
+
 
     $(".polaroid").each(function (i) {
         var tempVal = Math.round(Math.random());
